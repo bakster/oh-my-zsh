022d280303633077de1dc014e047f54e6ce69c51
Bakari Sumaila <bsumaila@gmail.com>
2012-11-03 00:03:37 +0100

    mac os elastic search plugin

---
+++ plugins/elasticsearch/elasticsearch.plugin.zsh
@@ -0,0 +1,4 @@
+alias elastic_start='elasticsearch -f -D es.config=/usr/local/Cellar/elasticsearch/0.19.7/config/elasticsearch.yml'
+alias elastic_stop='launchctl unload -wF ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist'
+alias elastic_log='open /usr/local/var/log/elasticsearch/elasticsearch_bakari.log'
+alias elastic_cluster='open /usr/local/var/elasticsearch/elasticsearch_bakari/'
